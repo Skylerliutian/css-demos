3aeb75f2499a3ac386015a1c898ca75573b96ee3
tian.liu <tian.liu@chaitin.com>
2022-09-19 14:41:43 +0800

    feat: new css effect

--- src/components/navbar1.jsx
+++ src/components/navbar1.jsx
@@ -1,60 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./static/home.svg";
 import Setting from "./static/setting.svg";
 import Message from "./static/message.svg";
-import Photo from "./static/photos.svg";
+import Photos from "./static/photos.svg";
 import Profile from "./static/profile.svg";
 import "./navbar1.css";
 
 const NavBar = () => {
+  const [current, setCurrent] = useState(0);
   const listMap = [
     {
-      component: <img src={Home} alt="home"></img>,
+      component: <img src={Home} alt="Home"></img>,
       name: "Home",
     },
+    {
+      component: <img src={Profile} alt="Profile"></img>,
+      name: "Profile",
+    },
+    {
+      component: <img src={Message} alt="Message"></img>,
+      name: "Message",
+    },
+    {
+      component: <img src={Photos} alt="Photos"></img>,
+      name: "Photos",
+    },
+    {
+      component: <img src={Setting} alt="Setting"></img>,
+      name: "Setting",
+    },
   ];
+  const handleClick = (index) => {
+    setCurrent(index);
+  };
   return (
     <div className="navigation">
       <ul>
-        <li className="list">
-          <a href="#javascript">
-            <span className="icon"></span>
-            <span className="text">Home</span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="#javascript">
-            <span className="icon">
-              <img src={Profile} alt="Profile"></img>
-            </span>
-            <span className="text">Profile</span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="#javascript">
-            <span className="icon">
-              <img src={Message} alt="Message"></img>
-            </span>
-            <span className="text">Message</span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="#javascript">
-            <span className="icon">
-              <img src={Photo} alt="Photos"></img>
-            </span>
-            <span className="text">Photos</span>
-          </a>
-        </li>
-        <li className="list">
-          <a href="#javascript">
-            <span className="icon">
-              <img src={Setting} alt="setting"></img>
-            </span>
-            <span className="text">Setting</span>
-          </a>
-        </li>
-        <div className="indicator"></div>
+        {listMap.map((item, index) => (
+          <li
+            className={`list ${index === current ? "active" : ""}`}
+            onClick={() => handleClick(index)}
+            key={index}
+          >
+            <a href="#javascript">
+              <span className="icon">{item.component}</span>
+              <span className="text">{item.name}</span>
+            </a>
+          </li>
+        ))}
+        <div className="indicator" style={{ "--left": current }}></div>
       </ul>
     </div>
   );
